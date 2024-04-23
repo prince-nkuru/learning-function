253eea4ccc62bf3684ab248e041dcb7c3960ba4e
prince-nkuru <nkuruprinces@gmail.com>
2024-04-22 22:33:42 -0700

    removed some contents

--- codewars/exercise-1.js
+++ codewars/exercise-1.js
@@ -1,82 +1,73 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <script>
-    // returning the digit of this number in an array in reversed order : 35231 to [1,3,2,5,3]
+// returning the digit of this number in an array in reversed order : 35231 to [1,3,2,5,3]
    
-      let number = 35231;
-      let convert = number.toString();
-      let opposite = convert.split("").reverse()//.map(convert.....);
-      console.log(opposite);
-      
-      /*2  My friend John and I are members of the "Fat to Fit Club (FFC)". John is worried because each month a list with the weights of members is published and each month he is the last on the list which means he is the heaviest.
+let number = 35231;
+let convert = number.toString();
+let opposite = convert.split("").reverse()//.map(convert.....);
+console.log(opposite);
 
-      I am the one who establishes the list so I told him: "Don't worry any more, I will modify the order of the list". It was decided to attribute a "weight" to numbers. The weight of a number will be from now on the sum of its digits.
+/*2  My friend John and I are members of the "Fat to Fit Club (FFC)". John is worried because each month a list with the weights of members is published and each month he is the last on the list which means he is the heaviest.
 
-      For example 99 will have "weight" 18, 100 will have "weight" 1 so in the list 100 will come before 99.
+I am the one who establishes the list so I told him: "Don't worry any more, I will modify the order of the list". It was decided to attribute a "weight" to numbers. The weight of a number will be from now on the sum of its digits.
 
-      Given a string with the weights of FFC members in normal order can you give this string ordered by "weights" of these numbers?
+For example 99 will have "weight" 18, 100 will have "weight" 1 so in the list 100 will come before 99.
 
-      Example:
-      "56 65 74 100 99 68 86 180 90" ordered by numbers weights becomes: 
+Given a string with the weights of FFC members in normal order can you give this string ordered by "weights" of these numbers?
 
-      "100 180 90 56 65 74 68 86 99"
-      When two numbers have the same "weight", let us class them as if they were strings (alphabetical ordering) and not numbers:
+Example:
+"56 65 74 100 99 68 86 180 90" ordered by numbers weights becomes: 
 
-      180 is before 90 since, having the same "weight" (9), it comes before as a string.
+"100 180 90 56 65 74 68 86 99"
+When two numbers have the same "weight", let us class them as if they were strings (alphabetical ordering) and not numbers:
 
-     All numbers in the list are positive numbers and the list can be empty.
+180 is before 90 since, having the same "weight" (9), it comes before as a string.
 
-     Notes
+All numbers in the list are positive numbers and the list can be empty.
+
+Notes
 
 */
 
-      function calculateWeight(num) {
-     return num.split('').reduce((sum, digit) => sum + parseInt(digit), 0);
-    }
+function calculateWeight(num) {
+return num.split('').reduce((sum, digit) => sum + parseInt(digit), 0);
+}
 
-   // Main function to sort the weights
-   function orderWeight(strng) {
-   if (!strng.trim()) {
-    return ""; // Handle empty or blank string case
-   }
+// Main function to sort the weights
+function orderWeight(strng) {
+if (!strng.trim()) {
+return ""; // Handle empty or blank string case
+}
 
-   const numbers = strng.trim().split(' ');
+const numbers = strng.trim().split(' ');
 
-   // Custom sorting by weight and then alphabetically as strings for ties
-    const sortedNumbers = numbers.sort((a, b) => {
-    const weightA = calculateWeight(a);
-    const weightB = calculateWeight(b);
+// Custom sorting by weight and then alphabetically as strings for ties
+const sortedNumbers = numbers.sort((a, b) => {
+const weightA = calculateWeight(a);
+const weightB = calculateWeight(b);
 
-     if (weightA === weightB) {
-      return a.localeCompare(b); // Alphabetical sorting if weights are the same
-     }
+if (weightA === weightB) {
+return a.localeCompare(b); // Alphabetical sorting if weights are the same
+}
 
-    return weightA - weightB; // Sorting by weights
-  });
+return weightA - weightB; // Sorting by weights
+});
 
-  return sortedNumbers.join(' '); // Return the sorted list as a string
+return sortedNumbers.join(' '); // Return the sorted list as a string
 }
 
 // second way 
-   
-function orderWeight(string) {  
-  var weights = string.split(' ');
-  
-  return weights.sort(function(a, b) {
-    var first = a.split('').reduce((x,y) => Number(x) + Number(y));
-    var second = b.split('').reduce((x,y) => Number(x) + Number(y));
-    return first - second === 0 ? a.localeCompare(b) : first - second; 
-  }).join(' ');
-}
- console.log(orderWeight('105 104 103 102'));
 
-    /* Write a function that takes a string of braces, and determines if the order of the braces is valid. It should return true if the string is valid, and false if it's invalid.
+function orderWeight(string) {  
+var weights = string.split(' ');
+
+return weights.sort(function(a, b) {
+var first = a.split('').reduce((x,y) => Number(x) + Number(y));
+var second = b.split('').reduce((x,y) => Number(x) + Number(y));
+return first - second === 0 ? a.localeCompare(b) : first - second; 
+}).join(' ');
+}
+console.log(orderWeight('105 104 103 102'));
+
+/* Write a function that takes a string of braces, and determines if the order of the braces is valid. It should return true if the string is valid, and false if it's invalid.
 
 This Kata is similar to the Valid Parentheses Kata, but introduces new characters: brackets [], and curly braces {}. Thanks to @arnedag for the idea!
 
@@ -93,22 +84,12 @@ Examples
 "[({})](]" =>  False;
 }*/
 
- function validBraces(braces){
-  while (braces.indexOf('{}') != - 1 || braces.indexOf('[]') != - 1 || braces.indexOf('()') != - 1) {
-    braces = braces.replace('{}', '').replace('()' ,'').replace('[]', '');
-  }
-  return braces.length == 0;
-  }
-  console.log(validBraces('[])({}){}'));
+function validBraces(braces){
+while (braces.indexOf('{}') != - 1 || braces.indexOf('[]') != - 1 || braces.indexOf('()') != - 1) {
+braces = braces.replace('{}', '').replace('()' ,'').replace('[]', '');
+}
+return braces.length == 0;
+}
+console.log(validBraces('[])({}){}'));
 
 
-
-
- 
-        
-
-       
-    
-  </script>
-</body>
-</html>
