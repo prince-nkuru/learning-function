f970a66ed34960f45d66a6d3a58b2e267d8c2ed9
prince-nkuru <nkuruprinces@gmail.com>
2024-05-07 23:14:56 -0700

    finalized the project

--- multiplicationApp/script.js
+++ multiplicationApp/script.js
@@ -1,18 +1,37 @@
  const input = document.querySelector('.input');
  const question =  document.querySelector('.question');
- const submit = document.querySelector('.submit');
- const score =  document.querySelector('.score');
+ const submiting = document.querySelector('.submit');
+ const scoreEI =  document.querySelector('.score');
+ const formEI = document.querySelector('.form');
+ let score = JSON.parse(localStorage.getItem('score'))
 
- const num1 = Math.round(Math.random() * 10);
- const num2 = Math.round(Math.random() * 10);
+ const num1 = Math.ceil(Math.random() * 10);
+ const num2 = Math.ceil(Math.random() * 10);
 
   const computerMove = num1 * num2;
 
-  submit.addEventListener('click' function(){
+  if(!score){
+    score = 0;
+  }
+
+  scoreEI.innerText = `score :${score}`;
    
-    question.innerHTML = `what is ${num1} multiply by ${num2}`
+  question.innerHTML =`what is ${num1} multiply by ${num2}`;
+ 
 
+  formEI.addEventListener('submit' ,() =>{
+    const userAns = +input.value;
+   
+     if (userAns === computerMove){
+      score++;
+      updateLocalStorage();
+     } else {
+      score--;
+      updateLocalStorage();
 
+     }
 
-
-  })
+    })
+    function updateLocalStorage () {
+      localStorage.setItem('score', JSON.stringify(score))
+     }
