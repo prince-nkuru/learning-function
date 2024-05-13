d99bc2c011d66f6af9fdda70aea1f59bd58163c8
prince-nkuru <nkuruprinces@gmail.com>
2024-05-12 23:01:36 -0700

    new file is commited

---
+++ codewars/diff-6kyu.js
@@ -0,0 +1,30 @@
+/*DESCRIPTION:
+Complete the solution so that it strips all text that follows any of a set of comment markers passed in. Any whitespace at the end of the line should also be stripped out.
+
+Example:
+
+Given an input string of:
+
+apples, pears # and bananas
+grapes
+bananas !apples
+The output expected would be:
+
+apples, pears
+grapes
+bananas
+The code would be called like so:
+
+var result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
+// result should == "apples, pears\ngrapes\nbananas"*/
+
+
+function solution(input, markers) {
+  let parts = input.split('\n');
+  for(let i = 0; i< parts.length; i++) {
+    markers.forEach(m => {
+      parts[i] = parts[i].includes(m) ? parts[i].substring(0, parts[i].indexOf(m)).trimEnd() : parts[i];
+    })
+  }
+  return parts.join('\n');
+};
