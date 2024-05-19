952bdc10249d9c88fb481a0d794916b732874575
prince-nkuru <nkuruprinces@gmail.com>
2024-05-18 23:31:08 -0700

    fourth way is added

--- codewars/length.js
+++ codewars/length.js
@@ -39,3 +39,20 @@ function findOdd(arr) {
   }
   return 0;
 }
+
+  // fourth way
+
+  function findOdd(A) {
+    var countOccurencesOfInt = 0;
+    for (let i = 0; i < A.length; i++) {
+        var currentIterationInt = A[i];
+        for (let j = 0; j < A.length; j++) {
+            if (currentIterationInt == A[j]) {
+                countOccurencesOfInt++;
+            }
+        }
+        if (countOccurencesOfInt % 2 != 0) {
+            return currentIterationInt;
+        }
+    }
+}
