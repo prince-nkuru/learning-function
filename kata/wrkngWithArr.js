e4b615e4fd604054b77584fb69a1b70c941aef9e
prince-nkuru <nkuruprinces@gmail.com>
2024-06-02 00:05:37 -0700

    commiting description

---
+++ kata/wrkngWithArr.js
@@ -0,0 +1,8 @@
+/*DESCRIPTION:
+In this kata the function returns an array/list of numbers without its last element. The function is already written for you and the basic tests pass, but random tests fail. Your task is to figure out why and fix it.
+
+Good luck!
+
+Hint: watch out for side effects.
+
+Some good reading: MDN Docs about arrays*/
