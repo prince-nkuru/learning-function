a27dbd11b2b021023774bd62c6299b3afc01456b
prince-nkuru <nkuruprinces@gmail.com>
2024-06-03 22:14:10 +0200

    second regexp is added

--- kata/regexp.js
+++ kata/regexp.js
@@ -38,3 +38,20 @@ function whichPostcode(postcode){
   if (/^\d\d\d \d\d$/i.test(postcode)) return 'SK'
   return 'Not valid'
 }
+
+// 2
+
+/*This kata requires you to write a function which merges two strings together. It does so by merging the end of the first string with the start of the second string together when they are an exact match.
+
+"abcde" + "cdefgh" => "abcdefgh"
+"abaab" + "aabab" => "abaabab"
+"abc" + "def" => "abcdef"
+"abc" + "abc" => "abc"
+NOTE: The algorithm should always use the longest possible overlap.
+
+"abaabaab" + "aabaabab" would be "abaabaabab" and not "abaabaabaabab"
+*/
+
+function mergeStrings(first, second){
+  return (first + ' ' + second).replace(/(.*) \1/, '$1');
+}
