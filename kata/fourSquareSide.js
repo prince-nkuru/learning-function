ae1ef72d34b48621e54ab30656ccef6ae3cc2ba7
prince-nkuru <nkuruprinces@gmail.com>
2024-06-05 21:26:39 +0200

    am adding its description

--- kata/fourSquareSide.js
+++ kata/fourSquareSide.js
@@ -1,3 +1,33 @@
+/*An Example
+Considering this square:
+
+    B A T S
+    A B U T
+    T U B A
+    S T A B
+Here are the four ways a word (in this case "TUBA") can be read:
+
+                         down
+                          ↓
+           B A T S    B A T S    B A T S    B A T S
+           A B U T    A B U T    A B U T    A B U T ← reverse
+  across → T U B A    T U B A    T U B A    T U B A
+           S T A B    S T A B    S T A B    S T A B
+                                   ↑
+                                   up
+IMPORTANT:
+
+In a true Sator Square, ALL of its words can be read in ALL four of these ways.
+If there is any deviation, it would be false to consider it a Sator Square.
+Some Details
+tablet (square) dimensions range from 2x2 to 33x33 inclusive
+all characters used will be upper-case alphabet letters
+there is no need to validate any input
+Input
+an N x N (square) two-dimentional matrix of uppercase letters
+Output
+boolean true or false whether or not the tablet is a Sator Square*/
+
 function isSatorSquare(tablet) {
   const across = []
   const down = []
