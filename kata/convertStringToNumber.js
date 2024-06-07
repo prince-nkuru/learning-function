ee6c60f2b01c58d57721b849e44b86243c2d1a18
prince-nkuru <nkuruprinces@gmail.com>
2024-06-07 17:05:50 +0200

    changing path of file and add new sol

--- kata/convertStringToNumber.js
+++ kata/convertStringToNumber.js
@@ -9,3 +9,10 @@ Create the function that takes as a parameter a sequence of numbers represented 
 ie:["1", "2", "3"] to [1, 2, 3]
 
 Note that you can receive floats as well.*/
+
+// first sol
+
+function toNumberArray(stringarray){
+  
+  return stringarray.map(n => parseFloat(n));
+}
