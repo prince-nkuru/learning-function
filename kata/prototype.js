e6bb05cd3fcca5036aad4d882756c02f1d6b0a5f
prince-nkuru <nkuruprinces@gmail.com>
2024-06-10 19:44:00 +0200

    prototype

---
+++ kata/prototype.js
@@ -0,0 +1,4 @@
+/*Create a function called reverse for the String prototype that will allow the following functionality:
+
+"String".reverse();// => returns "gnirtS"
+"Super awesome string".reverse();// => returns "gnirts emo*/
