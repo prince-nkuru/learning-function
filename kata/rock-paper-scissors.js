7c847540dbd0db47acf1c6dc14789cf1121a657e
prince-nkuru <nkuruprinces@gmail.com>
2024-06-10 22:01:12 +0200

    submitting second solution

--- kata/rock-paper-scissors.js
+++ kata/rock-paper-scissors.js
@@ -30,3 +30,20 @@ function rpsls(pl1, pl2) {
   return MATCH[pl1].indexOf(pl2) >= 0 ? 'Player 1 Won!' :
          MATCH[pl2].indexOf(pl1) >= 0 ? 'Player 2 Won!' : 'Draw!';
 }
+
+// second sol
+
+function rpsls(pl1, pl2) {
+  if (pl1 === pl2) return 'Draw!'
+  let rules = {
+      'paper': ['rock', 'spock'],
+      'rock': ['lizard', 'scissors'],
+      'lizard': ['spock', 'paper'],
+      'spock': ['scissors', 'rock'],
+      'scissors': ['lizard', 'paper']       
+  }
+  if (rules[pl1].includes(pl2))
+      return 'Player 1 Won!'
+  else
+      return 'Player 2 Won!'
+}
