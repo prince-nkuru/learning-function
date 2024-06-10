bd1747c0eb37860bfaac6c7eec505a1afbf32a90
prince-nkuru <nkuruprinces@gmail.com>
2024-06-10 19:59:41 +0200

    second solution

--- kata/prototype.js
+++ kata/prototype.js
@@ -6,3 +6,10 @@
 String.prototype.reverse = function(){
   return this.split('').reverse().join(''); 
 }
+
+// second sol
+
+//Create reverse function for the String prototype
+String.prototype.reverse = function(){
+  return this.length ? this.substring(1).reverse() + this.charAt(0) : "";
+}
