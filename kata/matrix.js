2ec9de065b6c762a2752f91193e39163296f245c
prince-nkuru <nkuruprinces@gmail.com>
2024-06-12 00:54:57 +0530

    alternative to previous kata

--- kata/matrix.js
+++ kata/matrix.js
@@ -48,3 +48,15 @@ function sumOfN(n) {
   }
   return arr;
 }
+
+// alternative
+
+function sumOfN(n) {
+  var array = [];
+  array[0] = 0;
+  for (var i = 1; i < Math.abs(n)+1; i++) {
+    if (n > 0) array[i] = array[i-1]+i;
+    else array[i] = array[i-1]-i;
+  }
+  return array;
+};
