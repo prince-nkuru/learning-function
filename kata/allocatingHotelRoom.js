4da372d25e0a33fcbb70943b9d2fa4bf7873d4aa
prince-nkuru <nkuruprinces@gmail.com>
2024-06-12 20:00:19 +0530

    submitting answer

--- kata/allocatingHotelRoom.js
+++ kata/allocatingHotelRoom.js
@@ -80,3 +80,16 @@ Every day you rent the car costs $40. If you rent the car for 7 or more days, yo
 Write a code that gives out the total amount for different days(d).
 
 */
+
+function rentalCarCost(d) {
+  let total = 0;
+  for (let i = 0; i < d; i++){
+    total += 40;
+  }
+  if (d >= 3 && d < 7){
+    total -= 20
+  }else if (d >= 7){
+    total -= 50;
+  }
+  return total 
+}
