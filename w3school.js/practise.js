4e4e652e58b51dc300c1b5b90de5d2e8d43c04ae
prince-nkuru <nkuruprinces@gmail.com>
2024-06-13 11:59:40 +0530

    making it complicated

--- w3school.js/practise.js
+++ w3school.js/practise.js
@@ -1,6 +1,7 @@
 let arr = [1,3,4,5,6,-9,-10,7];
 let maped = arr
             .filter(s => s >= 0)
-            .map(n => ({value: n}));
-
+            .map(n => ({value: n}))
+            .filter(t => t.value >= 6)
+            .map(n => n.value)
             console.log(maped);
