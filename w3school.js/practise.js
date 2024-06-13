c2c751461ed1f2c93732484ff16560d3dd6e6610
prince-nkuru <nkuruprinces@gmail.com>
2024-06-13 14:09:49 +0530

    adding more examples of reduce

--- w3school.js/practise.js
+++ w3school.js/practise.js
@@ -26,3 +26,22 @@ let maped = arr
   },0)
 
   console.log(result);
+
+  // reduce example 2
+  // convert two dimensional array into an objects
+
+  const keyValuePair = [
+    ['userName', 'prince'],
+    ['memberSince', '2024-04-05'],
+    ['age', 24]
+  ]
+
+  const final = keyValuePair.reduce((acc, curr) => {
+    let key = curr[0];
+    let value = curr[1];
+    acc[key] = value;
+
+    return acc;
+  }, {})
+
+  console.log(final);
