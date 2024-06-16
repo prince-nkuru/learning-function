2c9b13c22776811d7f306983b8794da9c131b254
prince-nkuru <nkuruprinces@gmail.com>
2024-06-16 20:56:43 +0530

    adding spot the difference answer

--- kata/spotDifference.js
+++ kata/spotDifference.js
@@ -15,3 +15,16 @@ NOTES:
 • Both strings will always be the same length
 
 • Capitalization and punctuation matter*/
+
+function spot(s1,s2){
+  let arr1 = s1.split('');
+  let arr2 = s2.split('');
+  let result = []
+  
+  for(let i = 0; i < arr1.length; i++){
+    if (arr1[i] != arr2[i]){
+      result.push(i)
+    }
+  }
+  return result
+}
