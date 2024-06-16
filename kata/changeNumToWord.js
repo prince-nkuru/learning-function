da7204750ad06652ce2b0af90670ade9172297dd
prince-nkuru <nkuruprinces@gmail.com>
2024-06-16 20:15:53 +0530

    adding a code that convert num to word

---
+++ kata/changeNumToWord.js
@@ -0,0 +1,6 @@
+/*DESCRIPTION:
+Wouldn't it be cool to be able to spell your numbers verbally, in digit-by-digit fashion? Instead of boring 731 you could write fancy Num.seven.three.one!
+
+Implement a class that allows chaining methods zero, one, two, three, four, five, six, seven, eight, nine to construct numbers in a similar manner.
+
+Note: your class should also support a method which converts its instances into integers (provided in the initial solution).*/
