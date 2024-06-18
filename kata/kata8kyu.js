094b61e056db626e41d9daf9657556ec70aed0c9
prince-nkuru <nkuruprinces@gmail.com>
2024-06-18 22:03:45 +0530

    answer to previous kata

--- kata/kata8kyu.js
+++ kata/kata8kyu.js
@@ -88,3 +88,10 @@ Write a program where Alex can input (n) how many times the hoop goes round and 
 
 If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
 If he doesn't get 10 hoops, return the string "Keep at it until you get it".*/
+
+function hoopCount (n) {
+  if (n >= 10)
+    return "Great, now move on to tricks"
+ else
+   return "Keep at it until you get it"
+}
