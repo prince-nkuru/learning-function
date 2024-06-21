37374da40ea32f339191b7ce217e6e4f9ac001c4
prince-nkuru <nkuruprinces@gmail.com>
2024-06-21 15:43:16 +0530

    last sol

--- kata/quineFunc.js
+++ kata/quineFunc.js
@@ -1,3 +1,5 @@
 // Quine is a nonempty program that prints itself. Your task is bit different than that. Write a function that takes no parameters and returns your program as a string instead of printing it.
 
 function quine(){ return `${quine}`; }
+
+const quine = () => { return "const quine = "+''+ quine }
