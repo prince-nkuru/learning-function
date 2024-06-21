e2ddfd0e5157d7e1d8f1d9b45ce7b040ae223512
prince-nkuru <nkuruprinces@gmail.com>
2024-06-21 11:48:40 +0530

    extending my code

--- DATASTRSUCTURE/stack.js
+++ DATASTRSUCTURE/stack.js
@@ -22,4 +22,29 @@ class stack {
     this.size++
   }
 
+  pop(){
+    if(this.size ===0) return null
+
+    const poppedNode = this.top;
+    this.top = this.top.next;
+    this.size--;
+    return poppedNode.val;
+
+    } 
+  
+
+  getTop(){
+    return this.top.val
+  }
+
 }
+
+const myStack = new stack();
+myStack.push('a');
+myStack.push('b');
+myStack.push('c');
+
+console.log(myStack.top);
+console.log(myStack.getTop());
+
+console.log(myStack.pop());
