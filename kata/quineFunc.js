56f926aeda48dc4d0dfbd384dfcc0dfc9f1ffa0e
prince-nkuru <nkuruprinces@gmail.com>
2024-06-21 15:25:52 +0530

    add quine function

---
+++ kata/quineFunc.js
@@ -0,0 +1,1 @@
+// Quine is a nonempty program that prints itself. Your task is bit different than that. Write a function that takes no parameters and returns your program as a string instead of printing it.
