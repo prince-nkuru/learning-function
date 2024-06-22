232c146ba39409ee7f37476ea17430667dad5a6c
prince-nkuru <nkuruprinces@gmail.com>
2024-06-22 21:58:33 +0530

    returning length of sequence

---
+++ kata/lengthOfSequence.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// As part of this kata, you need to find the length of the sub-array that begins and ends with the specified number.
+
+// For example, if the array arr is [0, -3, 7, 4, 0, 3, 7, 9], finding the length of the sub-array that begins and ends with 7s would return 5.
+
+// For sake of simplicity, there will only be numbers (positive or negative) in the supplied array.
+
+// If there are less or more than two occurrences of the number to search for, return 0.
