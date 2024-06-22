b327b2985f57b851548490788eacbbd70a5d5ee2
prince-nkuru <nkuruprinces@gmail.com>
2024-06-22 21:41:14 +0530

    add solution to non unique character

--- kata/duplicated.js
+++ kata/duplicated.js
@@ -5,3 +5,16 @@
 // input: "abccdefee"
 
 // output: "cceee"
+
+function onlyDuplicates(str) {
+  let arr = []
+  
+  let splitted = str.split('');
+  
+  for (let i = 0; i < splitted.length; i++){
+    if (splitted.indexOf(splitted[i]) !== splitted.lastIndexOf(splitted[i]) ){
+      arr.push(splitted[i])
+    }
+  }
+return arr.join('');
+}
