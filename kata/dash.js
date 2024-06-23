7731aa1ded37731af5f5a6e928cf80224cef34f4
prince-nkuru <nkuruprinces@gmail.com>
2024-06-23 21:44:58 +0530

    adding third option

--- kata/dash.js
+++ kata/dash.js
@@ -17,3 +17,23 @@ function dashatize(num) {
   }).join("").split("-").filter(a => a != "").join("-");
   
 };
+
+// third
+
+function dashatize(num) {
+  var dashed=Math.abs(num).toString().split('').map(a=> +a%2==1? '-'+a+'-':a).join('');
+  if (dashed[0]=='-') {
+    dashed=dashed.slice(1);
+  } 
+  if (dashed[dashed.length-1]=='-') {
+    dashed=dashed.slice(0,dashed.length-1);
+  }
+  for(var i=0;i<dashed.length;i++) {
+    if (dashed[i]=='-') {
+      if (dashed[i+1]=='-') {
+        dashed=dashed.substring(0,i+1)+dashed.substring(i+2);
+      }
+    }
+  }
+  return dashed;
+};
