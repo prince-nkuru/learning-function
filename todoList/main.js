f77269e269cb7af8375229cc33461f9b9fd95811
prince-nkuru <nkuruprinces@gmail.com>
2024-06-25 13:26:19 +0530

    adding functionality

--- todoList/main.js
+++ todoList/main.js
@@ -1,2 +1,30 @@
-alert('hello');
-console.log('hello')
+
+window.addEventListener('load', () => {
+const form = document.querySelector('#new-task-form');
+
+const input = document.querySelector('#new-task-input');
+
+const list_el = document.querySelector('#tasks');
+
+const task = input.Value;
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (!task){
+    alert('fill in');
+    return;
+  }
+
+  const task_el = document.createElement('div');
+  task_el.classList.add('task');
+
+  const task_content_el = document.createElement('div');
+  task_content_el.classList.add('content');
+  task_content_el.innerText = task;
+
+  task_el.appendChild(task_content_el);
+  list_el.appendChild(task_el);
+
+})
+})
