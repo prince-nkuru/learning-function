ac57390050e03152af553756a44455d8f651204d
prince-nkuru <nkuruprinces@gmail.com>
2024-06-26 21:56:36 +0530

    add solution to dice roll

--- kata/diceRoll.js
+++ kata/diceRoll.js
@@ -19,3 +19,12 @@
 //  2 4 4 5 4   450:  400 (for three 4s) + 50 (for the 5)
 // Note: your solution must not modify the input array.
 
+function score( dice ) {
+  var dc = [0,0,0,0,0,0];
+  var tdr = [1000,200,300,400,500,600];
+  var sdr = [100,0,0,0,50,0];
+  dice.forEach(function(x){ dc[x-1]++; });
+  return dc.reduce(function(s,x,i){ 
+    return s + (x >= 3? tdr[i] : 0) + sdr[i]*(x % 3);
+  },0);
+}
