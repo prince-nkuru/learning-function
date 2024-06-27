70725ccb88f1b4925fe00d809dbb2a64fa756ffb
prince-nkuru <nkuruprinces@gmail.com>
2024-06-27 20:31:48 +0530

    add probability task

---
+++ kata/probability.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Create a function which compares two probabilities, returning true if the first one is most likely otherwise false.
+
+// For this exercise probability is expressed as two numbers separated by a colon e.g. a probability of 1 in 3 will be 1:3.
+
+// So:
+
+// Input: ('1:3','1:2') - returns false as 1 in 3 is less likely than 1 in 2.
