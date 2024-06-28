b0550f9b6c2a228ab30db72f704c2cbfc63bd320
prince-nkuru <nkuruprinces@gmail.com>
2024-06-28 16:00:03 +0530

    answer to that

--- kata/sameLine.js
+++ kata/sameLine.js
@@ -3,3 +3,22 @@
 
 // onLine([[1,2], [7, 4], [22, 9]]);                 // returns true
 // onLine([[1,2], [-3, -14], [22, 9]]);              // returns false
+
+function onLine(points) {
+  var lhs, rhs;   
+  var isLinear = true;
+  
+  for(i=0; i<points.length-2;i++)
+  {
+      lhs = (points[i+1][1] - points[i][1])*(points[i+2][0] - points[i][0]);
+      rhs = (points[i+2][1] - points[i][1])*(points[i+1][0] - points[i][0]);
+      if(lhs != rhs)
+      {
+      isLinear = false;
+      break;
+      }
+  
+  }
+  
+  return isLinear;
+}
