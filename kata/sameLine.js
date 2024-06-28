77ff1624e7ef7f54bb8d66e6d16175d06b3ae270
prince-nkuru <nkuruprinces@gmail.com>
2024-06-28 15:59:07 +0530

    add if points are on the same line kata

---
+++ kata/sameLine.js
@@ -0,0 +1,5 @@
+// DESCRIPTION:
+// Given some points (cartesian coordinates), return true if all of them lie on a line. Treat both an empty set and a single point as a line.
+
+// onLine([[1,2], [7, 4], [22, 9]]);                 // returns true
+// onLine([[1,2], [-3, -14], [22, 9]]);              // returns false
