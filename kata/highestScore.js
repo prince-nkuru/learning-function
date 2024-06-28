d06c30e25cf4068a1dc61edbb491a08f56f78f8f
prince-nkuru <nkuruprinces@gmail.com>
2024-06-28 16:19:28 +0530

    add challenged solution

--- kata/highestScore.js
+++ kata/highestScore.js
@@ -10,3 +10,29 @@
 // If two words score the same, return the word that appears earliest in the original string.
 
 // All letters will be lowercase and all inputs will be valid.
+
+function high(x){
+  let alphabets = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+  ];
+  
+  let words = x.split(" ");
+  let highestScore = 0;
+  let highestScoreWord = "";
+  
+  for (let word of words) {
+    let lettersSum = 0;
+    
+    for (let letter of word) {
+      lettersSum += alphabets.indexOf(letter) + 1;
+    }
+    
+    if (lettersSum > highestScore) {
+      highestScore = lettersSum;
+      highestScoreWord = word;
+    }
+  }
+  
+  return highestScoreWord;
+}
