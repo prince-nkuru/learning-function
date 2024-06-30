5f8d026cf4ab3541c02c9dbdfbd99592abfb3b11
prince-nkuru <nkuruprinces@gmail.com>
2024-06-30 21:35:26 +0530

    add answer to ex file

---
+++ kata/ex.js
@@ -0,0 +1,20 @@
+// DESCRIPTION:
+// Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+// Examples:
+
+// * 'abc' =>  ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
+
+function solution(str){
+  var i = 0;
+  var result = new Array();
+  if (str.length % 2 !== 0) {
+    str = str + '_';
+  }
+  while (i < str.length) {
+      result.push(str[i] + str[i+1]);
+      i += 2;
+    }
+  return result;
+}
