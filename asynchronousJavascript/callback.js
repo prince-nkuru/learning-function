3d59c4dcf21fb0e84b963a04ef832edeb3a8412a
prince-nkuru <nkuruprinces@gmail.com>
2024-07-01 17:19:13 +0530

    add setInterval

--- asynchronousJavascript/callback.js
+++ asynchronousJavascript/callback.js
@@ -16,3 +16,17 @@ setTimeout(myFunction,3000);
 function myFunction(){
   console.log('i love you');
 }
+
+// When you pass a function as an argument, remember not to use parenthesis.
+
+// setInterval
+
+setInterval(myFunc,1000);
+let result = '';
+function myFunc(){
+  let times = new Date();
+ return result = times.getHours() + ' : ' +
+  times.getMinutes()+ ' : ' +
+  times.getMilliseconds()
+}
+console.log(result);
