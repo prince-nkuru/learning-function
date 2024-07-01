74f7462b81115caf0a0f6236506277f46aa6a0f3
prince-nkuru <nkuruprinces@gmail.com>
2024-07-01 18:01:34 +0530

    add more examples of async

--- asynchronousJavascript/async-await.js
+++ asynchronousJavascript/async-await.js
@@ -4,3 +4,26 @@
 
 // await makes a function wait for a Promise
 
+// The keyword async before a function makes the function return a promise:
+
+async function myFunctions (){
+  return 'hello'
+}
+//same as
+
+function myFunctions(){
+  return Promise.resolve('hello');
+}
+
+// example of how to use async
+
+function myDisplayer(some) {
+  document.getElementById("demo").innerHTML = some;
+}
+
+async function myFunction() {return "Hello";}
+
+myFunction().then(
+  function(value) {myDisplayer(value);},
+  function(error) {myDisplayer(error);}
+);
