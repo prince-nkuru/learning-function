c7e09c132ac893f1a4d0679ffab4562a5b3e0cbe
prince-nkuru <nkuruprinces@gmail.com>
2024-07-01 17:05:36 +0530

    adding setTimeout

--- asynchronousJavascript/callback.js
+++ asynchronousJavascript/callback.js
@@ -1,10 +1,18 @@
-function myDisplyer(something){
-  document.getElementById('').innerHTML = something;
-}
+// function myDisplyer(something){
+//   document.getElementById('').innerHTML = something;
+// }
 
-function calculate(num1, num2, myCallBack){
-  let sum = num1 + num2;
-  myCallBack(sum);
-}
+// function calculate(num1, num2, myCallBack){
+//   let sum = num1 + num2;
+//   myCallBack(sum);
+// }
 
-calculate(5,5, myDisplyer)
+// calculate(5,5, myDisplyer)
+
+// In the real world, callbacks are most often used with asynchronous functions. typical example is setTimeout.
+
+setTimeout(myFunction,3000);
+
+function myFunction(){
+  console.log('i love you');
+}
