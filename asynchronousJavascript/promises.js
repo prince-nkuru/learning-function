242f903da7628d0df99d9b85bbbb0e1d81f6faeb
prince-nkuru <nkuruprinces@gmail.com>
2024-07-01 17:31:17 +0530

    add some changes

--- asynchronousJavascript/promises.js
+++ asynchronousJavascript/promises.js
@@ -3,3 +3,15 @@
 // "Consuming code" is code that must wait for the result
 
 // A Promise is an Object that links Producing code and Consuming code
+
+let myPromise = new Promise(function(myResolve, myReject){
+// producing code (may take sometime)
+
+  myResolve(); // when successful
+  myReject(); // when failed
+})
+
+myPromise.then(
+  function(value){/*if code is successful*/},
+  function(error){/*if code is rejected*/}
+)
