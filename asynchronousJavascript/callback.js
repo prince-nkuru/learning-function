3c448196e035b7489f2ac2e909988d2b620bb5ed
prince-nkuru <nkuruprinces@gmail.com>
2024-07-01 16:57:24 +0530

    adding more infos about callback

--- asynchronousJavascript/callback.js
+++ asynchronousJavascript/callback.js
@@ -1,4 +1,10 @@
-function myDispalyer(something){
+function myDisplyer(something){
   document.getElementById('').innerHTML = something;
 }
 
+function calculate(num1, num2, myCallBack){
+  let sum = num1 + num2;
+  myCallBack(sum);
+}
+
+calculate(5,5, myDisplyer)
