763213eeb12e89715fb2518a987dec57c0c6103a
prince-nkuru <nkuruprinces@gmail.com>
2024-07-01 17:23:50 +0530

    start promises

---
+++ asynchronousJavascript/promises.js
@@ -0,0 +1,5 @@
+// "Producing code" is code that can take some time
+
+// "Consuming code" is code that must wait for the result
+
+// A Promise is an Object that links Producing code and Consuming code
