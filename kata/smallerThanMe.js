34d655413ce89e24c88425482a6e1e8f138b0132
prince-nkuru <nkuruprinces@gmail.com>
2024-07-02 20:16:47 +0530

    add description of smaller than me file

---
+++ kata/smallerThanMe.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Write a function that given, an array arr, returns an array containing at each index i the amount of numbers that are smaller than arr[i] to the right.
+
+// For example:
+
+// * Input [5, 4, 3, 2, 1] => Output [4, 3, 2, 1, 0]
+// * Input [1, 2, 0] => Output [1, 1, 0]
+
