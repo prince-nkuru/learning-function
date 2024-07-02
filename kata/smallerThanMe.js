6d93fca51790494988fff2ea0d9875dddcc5be90
prince-nkuru <nkuruprinces@gmail.com>
2024-07-02 20:34:11 +0530

    add answer

--- kata/smallerThanMe.js
+++ kata/smallerThanMe.js
@@ -6,3 +6,8 @@
 // * Input [5, 4, 3, 2, 1] => Output [4, 3, 2, 1, 0]
 // * Input [1, 2, 0] => Output [1, 1, 0]
 
+function smaller(nums) {
+  return nums.map((n, i) => {
+    return nums.slice(i).filter(v => v < n).length
+  });
+ }
