8081e18e7f9a3e4b38419cc238af7183fd5201c3
prince-nkuru <nkuruprinces@gmail.com>
2024-07-03 20:36:49 +0530

    second possible solution

--- kata/smallerThanMe.js
+++ kata/smallerThanMe.js
@@ -11,3 +11,19 @@ function smaller(nums) {
     return nums.slice(i).filter(v => v < n).length
   });
  }
+
+ // second
+ function smaller(nums) {
+  let count;
+  let base;
+  const arr = [];
+  for (let i = 0; i < nums.length; i++) {
+    base = nums[i];
+    count = 0;
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] < base) count++;
+    }
+    arr.push(count);
+  }
+  return arr;
+}
