1db94f39def0da6fe15688739c0c4828d8069fc9
prince-nkuru <nkuruprinces@gmail.com>
2024-07-04 12:35:38 +0530

    the start of async await method

--- asynchronousJavascript/async-await.js
+++ asynchronousJavascript/async-await.js
@@ -5,6 +5,7 @@
 // await makes a function wait for a Promise
 
 // The keyword async before a function makes the function return a promise:
+/// await is only used inside async function
 
 async function myFunctions (){
   return 'hello'
@@ -17,13 +18,26 @@ function myFunctions(){
 
 // example of how to use async
 
-function myDisplayer(some) {
-  document.getElementById("demo").innerHTML = some;
-}
+// function myDisplayer(some) {
+//   document.getElementById("demo").innerHTML = some;
+// }
 
-async function myFunction() {return "Hello";}
+// async function myFunction() {return "Hello";}
 
-myFunction().then(
-  function(value) {myDisplayer(value);},
-  function(error) {myDisplayer(error);} // remove this line if you dont expect an error.
-);
+// myFunction().then(
+//   function(value) {myDisplayer(value);},
+//   function(error) {myDisplayer(error);} // remove this line if you dont expect an error.
+// );
+
+  const p = new Promise((resolve,reject) => {
+    resolve('promise is returned')
+  })
+
+  // how to handle this promise using async and await
+  
+  async function handlePromise(){
+    const val = await p;
+    console.log(val)
+  }
+
+  handlePromise();
