1c1f3eebf390e8ac19bbcd897e32375a8b88c4a8
prince-nkuru <nkuruprinces@gmail.com>
2024-07-04 10:12:13 +0530

    add promise.allSettled

--- asynchronousJavascript/promiseAPIs.js
+++ asynchronousJavascript/promiseAPIs.js
@@ -1,5 +1,8 @@
+ //////////// for promise.all: fail first is returned. but if all succed, it return arr of all success
+
 const p1 = new Promise((resolve,reject) => {
-  setTimeout(() => resolve('p1 success'), 3000)
+  // setTimeout(() => resolve('p1 success'), 3000)
+  setTimeout(() => reject('p1 fail'), 3000)
 })
 
 const p2 = new Promise((resolve,reject) => {
@@ -8,7 +11,8 @@ const p2 = new Promise((resolve,reject) => {
 })
 
 const p3 = new Promise((resolve,reject) => {
-  setTimeout(() => resolve('p3 success'), 1000)
+  // setTimeout(() => resolve('p3 success'), 1000)
+  setTimeout(() => reject('p3 fail'), 1000)
 })
 
 Promise.all([p1,p2,p3])
@@ -18,3 +22,36 @@ Promise.all([p1,p2,p3])
 .catch((err) => {
   console.error(err)     // error keyword makes it look like error
 })
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//////     Promise.allSettled. SAFEST AMONG ALL. WAIT FOR ALL RESULT WEATHER RESOLVED OR REJECTED AND DISPLAY ARRAY.
+
+
+const p4 = new Promise((resolve,reject) => {
+  // setTimeout(() => resolve('p1 success'), 3000)
+  setTimeout(() => reject('p1 fail'), 3000)
+})
+
+const p5 = new Promise((resolve,reject) => {
+  // setTimeout(() => resolve('p2 success'), 2000)
+  setTimeout(() => reject('p2 fail'), 2000)
+})
+
+const p6 = new Promise((resolve,reject) => {
+  // setTimeout(() => resolve('p3 success'), 1000)
+  setTimeout(() => reject('p3 fail'), 1000)
+})
+
+Promise.allSettled([p4,p5,p6])
+.then((res) =>{
+  console.log(res)
+})
+.catch((err) => {
+  console.error(err)     // error keyword makes it look like error
+})
+
+/////////////////////// promise.race return the fast value, weather rejected or not the one with shorter time is returned
+
+
+
