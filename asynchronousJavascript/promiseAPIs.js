26e863e02674444a804d6bd6a3edfb1e36a83b17
prince-nkuru <nkuruprinces@gmail.com>
2024-07-04 09:54:34 +0530

    add promise.all

--- asynchronousJavascript/promiseAPIs.js
+++ asynchronousJavascript/promiseAPIs.js
@@ -3,9 +3,18 @@ const p1 = new Promise((resolve,reject) => {
 })
 
 const p2 = new Promise((resolve,reject) => {
-  setTimeout(() => resolve('p2 success'), 2000)
+  // setTimeout(() => resolve('p2 success'), 2000)
+  setTimeout(() => reject('p2 fail'), 2000)
 })
 
 const p3 = new Promise((resolve,reject) => {
   setTimeout(() => resolve('p3 success'), 1000)
+})
+
+Promise.all([p1,p2,p3])
+.then((res) =>{
+  console.log(res)
+})
+.catch((err) => {
+  console.error(err)     // error keyword makes it look like error
 })
