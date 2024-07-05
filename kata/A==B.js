d3d2555c749fd0ec699df38ec051ce7ffc0554d6
prince-nkuru <nkuruprinces@gmail.com>
2024-07-05 16:10:35 +0530

    add a==b file

---
+++ kata/A==B.js
@@ -0,0 +1,17 @@
+// DESCRIPTION:
+// Compare 2 integers and return if they are equals, don't use the comparison Operators
+
+// Can't use these Operators
+
+// Operator	Description
+// ==	equal to
+// ===	equal value and equal type
+// !=	not equal
+// !==	not equal value or not equal type
+// >	greater than
+// <	less than
+// >=	greater than or equal to
+// <=	less than or equal to
+// Example:
+//   0 == 0  => true 
+//   0 == 1  => false 
