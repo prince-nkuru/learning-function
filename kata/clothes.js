68898e8310be5a601f36b1adadee9be1eff8ab1c
prince-nkuru <nkuruprinces@gmail.com>
2024-07-13 22:32:21 +0530

    add solution

--- kata/clothes.js
+++ kata/clothes.js
@@ -7,3 +7,16 @@
 // let arr1 = [13, 64, 15, 17, 88];
 // let arr2 = [23, 14, 53, 17, 80];
 // getLargerNumbers(arr1, arr2); // Returns [23, 64, 53, 17, 88]
+
+function getLargerNumbers(a, b) {
+  let newArr = [];
+  
+ for (let i = 0; i < a.length; i++){
+   if (a[i] > b[i]){
+     newArr.push(a[i])
+   } else {
+     newArr.push(b[i])
+   }
+ }
+  return newArr;
+}
