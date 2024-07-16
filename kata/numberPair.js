a89f9eaa9be7909caeb886806c8d4ccc309f18ac
prince-nkuru <nkuruprinces@gmail.com>
2024-07-16 22:41:04 +0530

    add answer

--- kata/numberPair.js
+++ kata/numberPair.js
@@ -11,3 +11,7 @@
 // [1]         --> [1,1]
 // Remarks
 // All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
+
+function minMax(arr){
+  return [Math.min(...arr), Math.max(...arr)];
+}
