003ed48b2411044b631c5acff2db3271e9852de3
prince-nkuru <nkuruprinces@gmail.com>
2024-07-17 20:06:28 +0530

    submit answer

--- kata/trafficLight.js
+++ kata/trafficLight.js
@@ -4,3 +4,11 @@
 // Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
 
 // For example, when the input is green, output should be yellow.
+
+function updateLight(current) {
+  
+  if (current === 'green') return 'yellow';
+  if (current === 'yellow') return 'red';
+  if (current === 'red') return 'green';
+  
+}
