2ba6cb329fe05a94f93204a4dbf57431eec048d9
prince-nkuru <nkuruprinces@gmail.com>
2024-07-17 20:46:49 +0530

    add duplicate

---
+++ kata/duplicate.js
@@ -0,0 +1,5 @@
+// DESCRIPTION:
+// Please write a function that sums a list, but ignores any duplicated items in the list.
+
+// For instance, for the list [3, 4, 3, 6] the function should return 10,
+// and for the list [1, 10, 3, 10, 10] the function should return 4.
