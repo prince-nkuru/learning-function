282f26295a1e3f306f54b61acc27020fd436f878
prince-nkuru <nkuruprinces@gmail.com>
2024-07-22 21:34:10 +0530

    add first answer

--- kata/threeAndTwoo.js
+++ kata/threeAndTwoo.js
@@ -4,3 +4,10 @@
 // ["a", "a", "a", "b", "b"] ==> true  // 3x "a" and 2x "b"
 // ["a", "b", "c", "b", "c"] ==> false // 1x "a", 2x "b" and 2x "c"
 // ["a", "a", "a", "a", "a"] ==> false // 5x "a"
+function checkThreeAndTwo(array) {
+  let as = array.filter(x =>x === 'a').length;
+  let bs = array.filter(x =>x === 'b').length;
+  let cs = array.filter(x =>x === 'c').length;
+  return (as === 3 || bs === 3 || cs=== 3)  
+          && (as === 2 || bs === 2 || cs === 2);
+}
