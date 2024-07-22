927435c085c814029277bdc1586c5947103c5fb9
prince-nkuru <nkuruprinces@gmail.com>
2024-07-22 21:38:00 +0530

    add second answer

--- kata/threeAndTwoo.js
+++ kata/threeAndTwoo.js
@@ -11,3 +11,19 @@ function checkThreeAndTwo(array) {
   return (as === 3 || bs === 3 || cs=== 3)  
           && (as === 2 || bs === 2 || cs === 2);
 }
+
+////////////////////////////////////////////////
+
+function checkThreeAndTwo(array) {
+  var item1 = 0, item2 = 0, item3 = 0;
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === 'a') {
+      item1++;
+    } else if (array[i] === 'b') {
+      item2++;
+    } else if (array[i] === 'c') {
+      item3++;
+    }
+  }
+  return (item1 == 2 || item2 == 2 || item3 == 2) && (item1 == 3 || item2 == 3 || item3 == 3);
+}
