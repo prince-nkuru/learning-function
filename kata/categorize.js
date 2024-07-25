432e08972fd964259133b0f0c94140ed8d387fab
prince-nkuru <nkuruprinces@gmail.com>
2024-07-25 23:27:25 +0530

    add answer

--- kata/categorize.js
+++ kata/categorize.js
@@ -12,3 +12,7 @@
 // Example
 // input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
 // output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+
+function openOrSenior(data){
+  return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+}
