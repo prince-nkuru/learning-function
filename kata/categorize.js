ee828d90be20dea9d47650b72ea5eed4ea478fcb
prince-nkuru <nkuruprinces@gmail.com>
2024-07-25 23:29:10 +0530

    add second answer

--- kata/categorize.js
+++ kata/categorize.js
@@ -16,3 +16,12 @@
 function openOrSenior(data){
   return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+function openOrSenior(data){
+  function determineMembership(member){
+    return (member[0] >= 55 && member[1] > 7) ? 'Senior' : 'Open';
+  }
+  return data.map(determineMembership);
+}
