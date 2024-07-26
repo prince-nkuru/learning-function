e749347b4353e6e97a0cedfb48cd72d2351e83a4
prince-nkuru <nkuruprinces@gmail.com>
2024-07-26 14:51:25 +0530

    increasing answer

--- kata/uniqueOrder.js
+++ kata/uniqueOrder.js
@@ -10,3 +10,13 @@
 var uniqueInOrder=function(iterable){
   return [...iterable].filter((a, i) => a !== iterable[i-1])
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+var uniqueInOrder=function(iterable){
+  var res = [];
+  for (var i = 0; i < iterable.length; i++) {
+    if (iterable[i] != iterable[i+1]) res.push(iterable[i]);
+  }
+  return res;
+}
