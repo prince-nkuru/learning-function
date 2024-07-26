ef01bf6227679fe4bf7544381d49aa5a613d7c38
prince-nkuru <nkuruprinces@gmail.com>
2024-07-26 14:48:10 +0530

    add ans

--- kata/uniqueOrder.js
+++ kata/uniqueOrder.js
@@ -6,3 +6,7 @@
 // uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
 // uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 // uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+var uniqueInOrder=function(iterable){
+  return [...iterable].filter((a, i) => a !== iterable[i-1])
+}
