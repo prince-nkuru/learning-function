2610eab6349e17bae07c2bec065e83489771c19b
prince-nkuru <nkuruprinces@gmail.com>
2024-07-27 19:48:08 +0530

    added second answer

--- kata/expressionMatter.js
+++ kata/expressionMatter.js
@@ -21,4 +21,18 @@
 
 function expressionMatter(a, b, c) {
   return Math.max(a + b + c , (a + b)* c , a * (b + c) , a * b * c )
+
 }
+
+///////////////////////////////////////////////////////////////////
+
+function expressionMatter(a, b, c) {
+  let arr = [];
+  arr.push(a + b + c);
+  arr.push(a * b * c);
+  arr.push(a + b * c);
+  arr.push(a * b + c);
+  arr.push((a + b) * c);
+  arr.push(a * (b + c));
+  return arr.reduce((a,b) => Math.max(a, b))
+};
