9a45cd7dcb1472ef3a51a33d6c5b35957552788b
prince-nkuru <nkuruprinces@gmail.com>
2024-07-27 20:26:57 +0530

    add short answer

--- kata/fizzBuzz.js
+++ kata/fizzBuzz.js
@@ -25,3 +25,13 @@ Some examples
 "00:00"       "Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo"
 "12:00"       "Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo"
 Have fun!*/
+
+function fizzBuzzCuckooClock(time) {
+  let [hour, minute] = time.split(':');
+  return minute ==  0 ? "Cuckoo ".repeat(hour % 12 || 12).trim() :
+    minute      == 30 ? "Cuckoo" : 
+    minute % 15 ==  0 ? "Fizz Buzz" :
+    minute % 3  ==  0 ? "Fizz" :
+    minute % 5  ==  0 ? "Buzz" : 
+                        "tick";
+}
