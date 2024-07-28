428ae3d40db65a423fbe1cd99064766d4b44a4c7
prince-nkuru <nkuruprinces@gmail.com>
2024-07-28 21:06:32 +0530

    add sorting method to it

--- kata/minMax.js
+++ kata/minMax.js
@@ -8,3 +8,9 @@
 // [1, 434, 555, 34, 112] should return 554 (i.e., 555 - 1).
 
 // The array will contain a minimum of two elements. Input data range guarantees that max-min will cause no integer overflow.
+
+function betweenExtremes(numbers) {
+  let sorted = numbers.sort((a,b) => a - b);
+  
+  return sorted[sorted.length - 1] - sorted[0];
+}
