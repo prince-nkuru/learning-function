b988792175e3db2ccef667a12c2dcea31dd5dddf
prince-nkuru <nkuruprinces@gmail.com>
2024-07-29 18:52:47 +0530

    add second version answer to zodiac

--- kata/zodiac2.js
+++ kata/zodiac2.js
@@ -21,3 +21,21 @@ Depending on the cumulative score of the team, return the appropriate sentiment:
 <=80: 'kill me now'
 < 100 & > 80: 'i can handle this'
 100 or over: 'party time!!'*/
+
+function boredom(staff) {
+  var teams = { 'accounts'      : 1,
+                'finance'       : 2 ,
+                'canteen'       : 10, 
+                'regulation'    : 3, 
+                'trading'      : 6, 
+                'change'        : 6,
+                'IS'            : 8,
+                'retail'        : 5,
+                'cleaning'      : 4,
+                'pissing about' : 25},
+      score = Object.keys(staff)
+                    .map(key=>teams[staff[key]])
+                    .reduce((a,b) => a+b, 0);
+                    
+return score <= 80 ? 'kill me now' : (score > 100 ? 'party time!!' : 'i can handle this');            
+}
