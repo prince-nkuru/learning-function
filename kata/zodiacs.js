e3fb2eec47a4590666c854f8d64b5224453567bd
prince-nkuru <nkuruprinces@gmail.com>
2024-07-29 17:45:30 +0530

    add answer

--- kata/zodiacs.js
+++ kata/zodiacs.js
@@ -16,3 +16,14 @@
 // ['Wood', 'Fire', 'Earth', 'Metal', 'Water']
 
 // Tell me your zodiac sign and element in the comments. Happy coding :)
+
+function chineseZodiac(year){
+  let years =['Rat', 'Ox', 'Tiger', 'Rabbit', 'Dragon', 'Snake', 'Horse', 'Goat', 'Monkey', 'Rooster', 'Dog', 'Pig'];
+  let attribute = ['Wood', 'Fire', 'Earth', 'Metal', 'Water'];
+  let current = year - 1924;
+ 
+  let i = Math.floor(current%12); 
+  let j = Math.floor((current/2)%5);
+
+  return attribute[j] + ' '+years[i];
+}
