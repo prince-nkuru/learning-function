fc5fd49f69f793eddee3b4663130f33205360158
prince-nkuru <nkuruprinces@gmail.com>
2024-07-31 16:12:14 +0530

    added all

---
+++ kata/shift.js
@@ -0,0 +1,55 @@
+function moveNext(str) {
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let result = '';
+  let arr = str.split('');
+
+  for (let i = 0; i < arr.length; i++) {
+    if (alphabet.includes(arr[i])) {
+      let nextIndex = alphabet.indexOf(arr[i]) + 1;
+      // Wrap around if the character is 'z'
+      if (nextIndex >= alphabet.length) {
+        nextIndex = 0;
+      }
+      result += alphabet[nextIndex];
+    } else {
+      // Handle non-alphabet characters (optional)
+      result += arr[i];
+    }
+  }
+  
+  console.log(result);
+  return result;
+}
+
+moveNext('bcdef'); // Output: 'cdefg'
+
+
+function addNew(str){
+  if (str.startsWith('New')){
+    console.log(str)
+    return str;
+  }else{
+    console.log(`New ${str}`)
+    return `New ${str}`;
+  }
+}
+addNew('mess')
+
+
+const evenNumber = (num) => {
+  const arr = []
+  for (let i = 0; i <= num; i++){
+    if (i % 2 === 0){
+      arr.push(i);
+    }
+  }
+  console.log(arr.length);
+  return arr.length;
+}
+
+evenNumber(40);
+
+
+const isAscending = (arr) => arr.every((_, i) => i === 0 || arr[i -1] < arr[i]);
+
+console.log(isAscending([1,2,3,4,5,6]));
--- webAPI/formsApis.js
+++ webAPI/formsApis.js
@@ -15,7 +15,7 @@ customError	Set to true, if a custom validity message is set.
 patternMismatch	Set to true, if an element's value does not match its pattern attribute.
 rangeOverflow	Set to true, if an element's value is greater than its max attribute.
 rangeUnderflow	Set to true, if an element's value is less than its min attribute.
-stepMismatch	Set to true, if an element's value is invalid per its step attribute.
+stepMismatch	Set  to true, if an element's value is invalid per its step attribute.
 tooLong	Set to true, if an element's value exceeds its maxLength attribute.
 typeMismatch	Set to true, if an element's value is invalid per its type attribute.
 valueMissing	Set to true, if an element (with a required attribute) has no value.
