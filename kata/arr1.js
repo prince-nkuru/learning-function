071c40a4163e861de45da28e57a09bd8c700b915
prince-nkuru <nkuruprinces@gmail.com>
2024-08-02 09:14:29 +0530

    added data

---
+++ kata/arr1.js
@@ -0,0 +1,7 @@
+const people = [
+  {firstName:'prince', lastName: 'nkurunziza', department:'pharmacy', DOB: '06/09/2000',salary:1000},
+  {firstName:'levis', lastName: 'bayubahe', department:'pharmacy', DOB: '06/09/2001',salary:10002},
+  {firstName:'fbrice', lastName: 'niyibikora', department:'medicine', DOB: '05/09/2000',salary:4000},
+  {firstName:'janvier', lastName: 'niyonkuru', department:'pharmacy', DOB: '06/05/2001',salary:300}
+]
+
