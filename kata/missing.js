6d7b4f7198237036e22f2923e3b0a55b69e2e4bb
prince-nkuru <nkuruprinces@gmail.com>
2024-08-02 15:04:38 +0530

    added next

--- kata/missing.js
+++ kata/missing.js
@@ -19,3 +19,11 @@ function getMissingElement(superImportantArray){
   }
   
 }
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+function getMissingElement(superImportantArray){
+  for (i = 0; i < 10; i++) {
+    if (superImportantArray.indexOf(i) === -1) return i;
+  }
+}
