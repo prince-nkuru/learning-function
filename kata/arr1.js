e2a721fe33b7ea36109789998e94d9cc14ea3e88
prince-nkuru <nkuruprinces@gmail.com>
2024-08-02 09:23:00 +0530

    added first sol average of salaries

--- kata/arr1.js
+++ kata/arr1.js
@@ -1,7 +1,8 @@
 const people = [
-  {firstName:'prince', lastName: 'nkurunziza', department:'pharmacy', DOB: '06/09/2000',salary:1000},
-  {firstName:'levis', lastName: 'bayubahe', department:'pharmacy', DOB: '06/09/2001',salary:10002},
-  {firstName:'fbrice', lastName: 'niyibikora', department:'medicine', DOB: '05/09/2000',salary:4000},
-  {firstName:'janvier', lastName: 'niyonkuru', department:'pharmacy', DOB: '06/05/2001',salary:300}
+  {firstName:'prince', lastName: 'nkurunziza', department:'pharmacy', DOB: '06/09/2000',salary:'1000'},
+  {firstName:'levis', lastName: 'bayubahe', department:'pharmacy', DOB: '06/09/2001',salary:'10002'},
+  {firstName:'fbrice', lastName: 'niyibikora', department:'medicine', DOB: '05/09/2000',salary:'4000'},
+  {firstName:'janvier', lastName: 'niyonkuru', department:'pharmacy', DOB: '06/05/2001',salary:'300'}
 ]
-
+//1. average salaries
+//people.reduce((prev, curr) => {return (prev + parseInt(curr.salary))/people.length},0)
