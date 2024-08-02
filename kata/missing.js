afc00fbf343805113627cd9605bae40e9dda9a35
prince-nkuru <nkuruprinces@gmail.com>
2024-08-02 15:09:15 +0530

    added last

--- kata/missing.js
+++ kata/missing.js
@@ -33,3 +33,10 @@ function getMissingElement(superImportantArray){
 function getMissingElement(superImportantArray) {
   return superImportantArray.reduce(function (sum, value) {return sum - value;}, 45);
 }
+//////////////////////////////////////////////////////////////////////////////////////
+
+function getMissingElement(superImportantArray){
+  return [0,1,2,3,4,5,6,7,8,9].filter(function(n) {
+    return superImportantArray.indexOf(n) === -1;
+  })[0];
+}
