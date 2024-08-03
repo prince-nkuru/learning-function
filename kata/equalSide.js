8fe3c77ec5e7759649a6ccf78d9ba1d63a12ae07
prince-nkuru <nkuruprinces@gmail.com>
2024-08-03 15:55:49 +0530

    submitted answer

--- kata/equalSide.js
+++ kata/equalSide.js
@@ -9,3 +9,8 @@ Notes
 All numbers are valid Int32, no need to validate them.
 There will always be at least one number in the input string.
 Output string must be two numbers separated by a single space, and highest number is first.*/
+
+function highAndLow(numbers){
+  numbers = numbers.split(' ');
+  return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+}
