d3182d8654efefcabea9731a55082c5a9ec436b6
prince-nkuru <nkuruprinces@gmail.com>
2024-08-04 22:01:14 +0530

    added sol

--- kata/divBy3.js
+++ kata/divBy3.js
@@ -12,3 +12,22 @@ NOTE: In Rust, the bounds are (2 ≤ n ≤ 2^51), (20 ≤ m ≤ 2^52).
 
 Output:
 result - array of integers*/
+
+let possibleNumbers = []
+
+function solution(n, m) {
+  if (!possibleNumbers.length) {
+    for (let i = 2n; i <= 31622n; i++) {
+      if(isPrime(i)) possibleNumbers.push(i**4n)
+    }
+  }
+  
+  return possibleNumbers.filter(number => number >= n && number <= m);
+}
+
+function isPrime(p) {
+  for (let i = 2n; i * i <= p; i++) {
+    if (p % i === 0n) return false;
+  }
+  return true;
+}
