f66feab32f2cc17940befdabd0205424297e4855
prince-nkuru <nkuruprinces@gmail.com>
2024-09-16 20:20:54 +0530

    added ans

--- kata/fibonacc.js
+++ kata/fibonacc.js
@@ -3,3 +3,11 @@
 // a is the first term of a Fibonacci sequence, b is the second term of this sequence ( it will always be bigger than or equal to a and they'll be both always positive, so don't worry about pesky input checking ) and f is a number which you'll need to check whether or not is a part of this Fibonacci sequence.
 
 // Good luck, muh fellow coding warrior.
+function isFibo(a,b,f) {
+  while (a < f) {
+    const t = a;
+    a = b;
+    b = t + b;
+  }
+  return a == f;
+}
