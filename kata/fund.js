6f1727fea678d50f4b81fb0c23afc1e78ed6a7d5
prince-nkuru <nkuruprinces@gmail.com>
2024-09-20 16:42:33 +0530

    add first ans

--- kata/fund.js
+++ kata/fund.js
@@ -11,3 +11,18 @@ Example
 { 1, 1, 11, 2, 3 } => 6
 Input validation
 If an empty value ( null, None, Nothing, nil etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.*/
+
+function sumArray(array) {
+  if (array == null) {
+    return 0;
+  } else if (array.length < 2) {
+    return 0;
+  } else {
+    array = array.sort(function(a,b) {return a - b;});
+    var total = 0;
+    for (var i = 1; i < array.length - 1; i++) {
+      total += array[i];
+    }
+    return total;
+  }
+}
